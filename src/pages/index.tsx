a7e38878b1e546ff3b23243bb807815a8219f36c
Gabriel Kenji Poli Ozaki <g.kenjiJS@hotmail.com>
2022-09-26 17:35:33 -0300

    Feat: Adicionando os Cards em tela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,9 +62,18 @@ export default function Home(): JSX.Element {
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button 
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
